d221ff40c0312cb8ac45cdedec56991291edb14f
Tamás Matyó <tomi.matyo@gmail.com>
2023-09-27 23:57:50 +0200

    styling and fe input validation added

--- resources/js/Pages/App.vue
+++ resources/js/Pages/App.vue
@@ -1,13 +1,13 @@
 <template>
     <section>
         <div class="container form">
-            <h2>Hi, lets shorten that URL!</h2>
+            <h2>{{ tr.title }}</h2>
             <form v-on:submit.prevent="shortenTheUrl">
                 <div class="row">
                     <input v-model="url" type="text" name="url" id="url" placeholder="Your URL goes here">
-                    <input type="submit" :value="tr.submit" id="cta">
+                    <input type="submit" :value="tr.submit" id="cta" :disabled="!isFilled">
                 </div>
-                <p>Short URL visible: {{ showShortUrl }}</p>
+                <label for="url" v-if="showError" class="url-error">{{ tr.errors.notUrl }}</label>
                 <p v-if="showShortUrl">{{ shortUrl }}</p>
             </form>
         </div>
@@ -21,27 +21,51 @@ export default {
         return {
             url: '',
             shortUrl: '',
+            showError: false,
             tr: {
-                submit: 'Submitttt'
+                title: 'Hi, lets shorten that URL!',
+                submit: 'Submitttt',
+                errors: {
+                    notUrl: 'This is not a valid URL.'
+                }
             }
         }
     },
     computed: {
         isFilled() {
-            return this.url.length > 0;
+            return this.url.length > 0
         },
         showShortUrl() {
-            return this.shortUrl.length > 0;
+            return this.shortUrl.length > 0
+        },
+        isUrl() {
+            try {
+                let u = new URL(this.url)
+                return true && (u.protocol === 'http:' || u.protocol === 'https:')
+            } catch (error) {
+                return false
+            }
         }
     },
     methods: {
         shortenTheUrl() {
+            if(!this.isFilled) {
+                return
+            }
+
+            if(!this.isUrl) {
+                this.showError = true
+                return
+            }
+
             var self = this;
             Axios.post('/shortenTheUrl', { 
                 url: this.url
             }).then((res) => {
-                self.shortUrl = res.data.shortUrl;
-                console.log(res);
+                if(res.status === 200) {
+                    self.shortUrl = res.data.shortUrl
+                }
+                console.log(res)
             }).catch( e => console.log(e))
         }
     }
@@ -91,11 +115,22 @@ export default {
                     color: var(--base);
                     border: none;
 
-                    &:hover {
+                    &:hover:enabled{
                         cursor: pointer;
                         filter: brightness(110%);
                     }
+
+                    &[disabled] {
+                        /* background: var(--grey);
+                        background-color: var(--grey); */
+                        display: none;
+                    }
                 }
+            }
+
+            .url-error {
+                margin: 15px;
+                display: block;
             }
         }
     }
