043b077a51b7b5f8d0e38afd4623dffeb4fba115
Tamás Matyó <tomi.matyo@gmail.com>
2023-10-26 00:19:10 +0200

    added modal for 2x confirm before URL removal

--- resources/js/Components/UserUrlList.vue
+++ resources/js/Components/UserUrlList.vue
@@ -5,6 +5,9 @@ const urls = ref(0)
 const toggleResponse = ref(0)
 const deleteResponse = ref(false)
 
+const urlId = ref(0)
+const toggleModalWindow = ref(false)
+
 onMounted(() => loadData())
 
 const loadData = async () => {
@@ -31,7 +34,7 @@ const toggleActiveUrl = async (url_id) => {
     toggleResponse.value.ok ? loadData() : console.log("summfink vent vonk")
 }
 
-const deleteShortUrl = async (url_id) => {
+const deleteShortUrl = async (url_id = urlId.value) => {
     deleteResponse.value = await fetch('/deleteUrl', { 
         headers: {
             'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]').content
@@ -40,7 +43,23 @@ const deleteShortUrl = async (url_id) => {
         body: JSON.stringify({url_id})
     }).then(res => res.json())
 
+    if(toggleModalWindow.value) {
+        closeModal()
+    }
+
     deleteResponse.value.ok ? loadData() : console.log("summfink vent vonk")
+}
+
+const openModal = (id) => {
+    if(id > 0) {
+        urlId.value = id
+        toggleModalWindow.value = true
+        console.log(toggleModalWindow.value)
+    }
+}
+const closeModal = () => {
+    urlId.value = 0
+    toggleModalWindow.value = false
 }
 
 </script>
@@ -83,7 +102,7 @@ const deleteShortUrl = async (url_id) => {
                             <td class="border border-slate-200 p-4 text-slate-500 dark:text-slate-400 text-center">{{ dateBuilder(u.updated_at) }}</td>
                             <td class="border border-slate-200 p-4 text-slate-500 dark:text-slate-400 text-center">{{ u.stat_count }}</td>
                             <td class="border border-slate-200 p-4 text-slate-500 dark:text-slate-400 text-center">
-                                <a href="javascript:void()" class="text-center align-middle" @click="deleteShortUrl(u.id)">
+                                <a href="#" class="text-center align-middle" @click="openModal(u.id)" >
                                     <svg class="h-6 w-6 text-red-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" />  <line x1="15" y1="9" x2="9" y2="15" />  <line x1="9" y1="9" x2="15" y2="15" /></svg>
                                 </a>
                             </td>
@@ -93,6 +112,31 @@ const deleteShortUrl = async (url_id) => {
             </div>
         </div>
     </div>
+
+    <div id="popup-modal-bg" class="fixed top-0 left-0 right-0 w-full h-full bg-black dark:bg-black bg-opacity-50 dark:bg-opacity-50 z-49 " v-if="toggleModalWindow"></div>
+    <div id="popup-modal" tabindex="-1" class="fixed flex items-center justify-center z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full" v-if="toggleModalWindow">
+        <div class="relative w-full max-w-md max-h-full">
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <button type="button" @click="closeModal()" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+                <div class="p-6 text-center">
+                    <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                    </svg>
+                    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
+                    <button @click="deleteShortUrl()" data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                        Yes, I'm sure
+                    </button>
+                    <button @click="closeModal()" data-modal-hide="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <style>
