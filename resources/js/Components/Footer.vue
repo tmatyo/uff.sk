d9913dcd31e507c32074ae66b560eb6559822a1e
Tamás Matyó <tomi.matyo@gmail.com>
2023-11-04 00:17:07 +0100

    logo adjustments

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -11,7 +11,7 @@ import { Link } from '@inertiajs/vue3';
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <Link :href="route('dashboard')">
-                        <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
+                        <ApplicationLogo class="block h-14 w-auto fill-current text-gray-800" />
                     </Link>
                 </div>
                 <!-- Footer menu -->
